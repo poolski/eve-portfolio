e3374d5f1a7aaec3f52ea11613d8f46571ce0db0
Kyrill <kyrill.poole@gmail.com>
2012-08-04 16:34:00 +0100

    removed the corresponding wallet balance thing from here

--- application/views/account/list_registered_characters.php
+++ application/views/account/list_registered_characters.php
@@ -1,8 +1,5 @@
 <?php foreach ($characters as $char): ?>
     <?php if(is_array($char) && array_key_exists('name', $char)) {?>
-    	<a href="<?php echo base_url();?>character/detail/<?php echo($char['characterID']);?>"><h2><?php echo($char['name']); ?></h2></a>
-        <p>
-        	<?php echo("Wallet balance: ".$char['balance']." ISK");?>
-        </p>
+    	<a href="<?php echo base_url();?>character/detail/<?php echo($char['characterID']);?>"><p><?php echo($char['name']); ?></p></a>
     <?php }?>
 <?php endforeach ?>
